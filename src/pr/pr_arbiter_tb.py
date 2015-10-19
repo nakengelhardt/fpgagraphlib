121c2b13cca91a7fc64a7a30da27034fd0eab1ba
N. Engelhardt <nakengelhardt@gmail.com>
2015-10-19 16:48:43 +0800

    move arbiter to new migen

--- src/pr/pr_arbiter_tb.py
+++ src/pr/pr_arbiter_tb.py
@@ -1,92 +1,90 @@
-from migen.fhdl.std import *
-from migen.genlib.fifo import SyncFIFO
-from migen.sim.generic import *
+import unittest
 
-from pr_interfaces import PRMessage
+from migen import *
+from tbsupport import *
+
+from pr_interfaces import _msg_layout
 from pr_arbiter import PRArbiter
 from pr_address import PRAddressLayout
 from pr_config import config
-
-class FifoWriter(Module):
-	def __init__(self, fifos, messages):
-		self.fifos = fifos
-		self.messages = messages 
-
-	def gen_simulation(self, selfp):
-		# send inputs
-		msgs_sent = 0
-		for i in range(len(self.fifos)):
-				selfp.fifos[i].we = 0
-				selfp.fifos[i].din.barrier = 0
-		while msgs_sent < len(self.messages):
-			dest_id, parent = self.messages[msgs_sent]
-			pe = dest_id % len(self.fifos)
-			if selfp.fifos[pe].writable:
-				print("Sending message " + str((dest_id, parent)) + " on fifo " + str(pe))
-				selfp.fifos[pe].din.dest_id = dest_id
-				selfp.fifos[pe].din.payload = parent
-				selfp.fifos[pe].we = 1
-				msgs_sent += 1
-			yield
-			for i in range(len(self.fifos)):
-				selfp.fifos[i].we = 0
-		yield
-		for i in range(len(self.fifos)):
-				selfp.fifos[i].we = 1
-				selfp.fifos[i].din.barrier = 1
-		yield
-		for i in range(len(self.fifos)):
-				selfp.fifos[i].we = 0
-				selfp.fifos[i].din.barrier = 0
+from recordfifo import RecordFIFO
 
 
+class ArbiterCase(SimCase, unittest.TestCase):
+    class TestBench(Module):
+        def __init__(self):
 
-class TB(Module):
-	def __init__(self):
+            self.addresslayout = config(quiet=True)
 
-		self.addresslayout = config()
+            num_pe = self.addresslayout.num_pe
 
-		num_pe = self.addresslayout.num_pe
+            msg_format = set_layout_parameters(_msg_layout, **self.addresslayout.get_params())
+            
+            self.fifos = [RecordFIFO(layout=msg_format, depth=32) for _ in range(num_pe)]
+            self.submodules += self.fifos
 
-		fifos = [SyncFIFO(width_or_layout=PRMessage(**self.addresslayout.get_params()).layout, depth=32) for _ in range(num_pe)]
-		self.submodules += fifos
+            self.submodules.dut = PRArbiter(self.addresslayout, self.fifos)
 
-		self.submodules.dut = PRArbiter(self.addresslayout, fifos)
+            self.messages_send = [(1, 171753), (2, 84960), (3, 80667), (4, 78659), (5, 34255), (6, 93813), (7, 132367)]
+            self.messages = self.messages_send.copy()
+    
+    def test_arbiter(self):
+        def gen_input():
+            # send inputs
+            for i in range(len(self.tb.fifos)):
+                    yield self.tb.fifos[i].we.eq(0)
+                    yield self.tb.fifos[i].din.barrier.eq(0)
+            while self.tb.messages_send:
+                dest_id, parent = self.tb.messages_send.pop()
+                pe = dest_id % len(self.tb.fifos)
+                # print("Sending message " + str((dest_id, parent)) + " on fifo " + str(pe))
+                yield self.tb.fifos[pe].din.dest_id.eq(dest_id)
+                yield self.tb.fifos[pe].din.payload.eq(parent)
+                yield self.tb.fifos[pe].we.eq(1)
+                yield
+                while not (yield self.tb.fifos[pe].writable):
+                    yield
+                yield self.tb.fifos[pe].we.eq(0)
+                # print("Messages left: " + str(len(self.tb.messages_send)))
+            yield
+            for i in range(len(self.tb.fifos)):
+                    yield self.tb.fifos[i].we.eq(1)
+                    yield self.tb.fifos[i].din.barrier.eq(1)
+            yield
+            for i in range(len(self.tb.fifos)):
+                    yield self.tb.fifos[i].we.eq(0)
+                    yield self.tb.fifos[i].din.barrier.eq(0)
 
-		self.messages = [(1, 171753), (2, 84960), (3, 80667), (4, 78659), (5, 34255), (6, 93813), (7, 132367)]
-		
-		# delegate input to submodule
-		self.submodules += FifoWriter(fifos, self.messages.copy()) # messages modified afterwards!
+        def gen_output():
+            # check output
+            # TODO: add testing of pipeline stall by sometimes turning ack off?
+            yield self.tb.dut.apply_interface.ack.eq(1)
+            yield self.tb.dut.start_message.select.eq(0)
+            
+            msgs_received = 0
+            total_msgs = len(self.tb.messages)
+            # print("Total messages: " + str(total_msgs))
+            while msgs_received < total_msgs:
+                if (yield self.tb.dut.apply_interface.valid):
+                    self.assertFalse((yield self.tb.dut.apply_interface.msg.barrier))
+                    msg = ((yield self.tb.dut.apply_interface.msg.dest_id), (yield self.tb.dut.apply_interface.msg.payload))
+                    # print("{0:{1}d}: {2}".format(msgs_received, len(str(total_msgs-1)), msg))
+                    try:
+                        self.tb.messages.remove(msg)
+                        msgs_received += 1
+                    except ValueError as e:
+                        self.fail(msg="Unexpected message received: " + str(msg))
+                yield
+            if self.tb.messages:
+                self.fail(msg="Messages not received: " + str(self.tb.messages))
+            for i in range(20):
+                if (yield self.tb.dut.apply_interface.valid) & (yield self.tb.dut.apply_interface.msg.barrier):
+                    break
+                yield
+            else:
+                self.fail("Barrier not reached")
 
-	def gen_simulation(self, selfp):
-		# check output
-		# TODO: add testing of pipeline stall by sometimes turning ack off?
-		selfp.dut.apply_interface.ack = 1
-		selfp.dut.start_message.select = 0
-		
-		msgs_received = 0
-		total_msgs = len(self.messages)
-		print("Total messages: " + str(total_msgs))
-		while msgs_received < total_msgs:
-			if selfp.dut.apply_interface.valid:
-				msg = (selfp.dut.apply_interface.msg.dest_id, selfp.dut.apply_interface.msg.payload)
-				txt = "{0:{1}d}: ".format(msgs_received, len(str(total_msgs-1))) + str(msg)
-				try:
-					self.messages.remove(msg)
-					msgs_received += 1
-				except ValueError as e:
-					txt += " ! unexpected"
-				print(txt)
-			yield
-		if self.messages:
-			print("Messages not received: " + str(self.messages))
-		else:
-			print("All messages received.")
-		if selfp.dut.apply_interface.valid & selfp.dut.apply_interface.msg.barrier:
-			print("Barrier reached.")
+        self.run_with([gen_input(), gen_output()], vcd_name="tb.vcd")
 
 if __name__ == "__main__":
-	tb = TB()
-	# run_simulation(tb, vcd_name="tb.vcd", ncycles=250)
-	with Simulator(tb, TopLevel("tb.vcd"), icarus.Runner(keep_files=True), display_run=True) as s:
-		s.run(250)
+    unittest.main()
