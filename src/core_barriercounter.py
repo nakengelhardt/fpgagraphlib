nakengelhardt/fpgagraphlib

commit 6ab66fd44143029175392375c76e732c9214a5af

skip checking again that all barriers received after waiting for stragglers

src/core_barriercounter.py
@@ -111,6 +111,6 @@ class Barriercounter(Module):
                 If(apply_interface_in_fifo.dout.valid,
                     NextValue(self.num_from_pe[config.addresslayout.pe_adr(apply_interface_in_fifo.dout.msg.sender)], self.num_from_pe[config.addresslayout.pe_adr(apply_interface_in_fifo.dout.msg.sender)] + 1)
                 ),
-                NextState("CHK_BARRIER")
+                NextState("PASS_BARRIER")
             )
         )

commit afc85219160ad7ac051404a130e189adf29d9074

revert 6ab66fd which introduced a bug

src/core_barriercounter.py
@@ -110,13 +110,7 @@ class Barriercounter(Module):
                 NextValue(self.apply_interface_out.valid, apply_interface_in_fifo.dout.valid),
                 If(apply_interface_in_fifo.dout.valid,
                     NextValue(self.num_from_pe[config.addresslayout.pe_adr(apply_interface_in_fifo.dout.msg.sender)], self.num_from_pe[config.addresslayout.pe_adr(apply_interface_in_fifo.dout.msg.sender)] + 1),
-                    NextState("WAIT_CNT_UPDATE")
+                    NextState("CHK_BARRIER") #this gratuitously checks all_barriers_recvd again, but we need to wait an extra cycle for all_messages_recvd to be updated
                 )
             )
-        )
-
-        self.fsm.act("WAIT_CNT_UPDATE",
-        # self.all_messages_recvd is updated with an extra cycle delay
-        # so have to wait 1 cycle before going to check
-            NextState("PASS_BARRIER")
         )